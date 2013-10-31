0025af4b902c9da210534a930e91f4a22f08fdce
cchantep <chantepie@altern.org>
2013-10-31 01:15:10 +0100

    Initial commit/doc

--- README.md
+++ README.md
@@ -1,4 +1,42 @@
-bootstrap-combobox
-==================
+# Bootstrap Combobox plugin
 
-Combobox plugin for Twitter Bootstrap
+Supports combobox as a Twitter Bootstrap component, 
+based on [dropdown](http://getbootstrap.com/components/#dropdowns).
+
+## JavaScript usage
+
+```javascript
+$('.btn-group').btComboBox()
+```
+
+### Methods
+
+#### .btComboBox('selectedOption')
+Returns selected option (`[val, label, element]`) or `null` (if none).
+
+```javascript
+var o = $('.btn-group').btComboBox('selectedOption');
+var val = o[0], label = o[1], elem = o[2];
+```
+
+#### .btComboBox('clear')
+Removes all options and unselect value.
+
+```javascript
+$('.btn-group').btComboBox('clear')
+```
+
+#### .btComboBox({'action':"load", parameters})
+
+Appends options, using given `parameters` properties.
+
+* `pairs`: Array of option to be appended; Each array element 
+should be either `[val,label]`, or anything else if next `extractor` 
+property is provided.
+* `extractor` (optional, required if elements of `pairs` array 
+aren't `[val,label]`): Function `(element of pairs) => [value, label]`.
+
+#### .btComboBox({'action':"reset", parameters})
+
+Reset options, using same `parameters` as load method.
+Keep value selection (if value is still available in new options).
---
+++ bootstrap.combobox.js
@@ -0,0 +1,158 @@
+(function ($) {
+    "use strict";
+
+    $.fn.btComboBox = function(arg) {
+        var selectedOption = function(g) {
+            var o = $(".active a", g);
+
+            if (o.length) {
+                return [o.attr("href"),o.text(),o]
+            }
+
+            // ---
+
+            var f = $("li:first-child a", g);
+
+            return (f.length) ? [f.attr("href"),f.text(),f] : null
+        };
+
+        if (arg == "selectedOption") {
+            return selectedOption($(this))
+        }
+
+        var removeAll = function(e) { 
+            $("li", e).remove();
+            return e
+        };
+        var selectValue = function(id, g, b, a) {
+            var v = (a) ? a.attr("href") : null, 
+            l = (a) ? a.text() : "";
+
+            $("#"+id+"-hid").val(v);
+            $(".label", b).text(l);
+
+            $(".active", g).removeClass("active");
+
+            if (a) a.parent().addClass("active");
+
+            return false
+        };
+
+        if (arg == "clear") {
+            return $(this).each(function(i,e) { 
+                var g = $(e), id = g.attr("id"), b = $(".btn", g);
+                removeAll(g);
+                selectValue(id, g, b, null)
+            })
+        };
+
+        // ---
+        
+        var options = function(pairs, extractor) {
+            var l;
+            if (!pairs || !(l = pairs['length'])) { 
+                // No pair
+                return;
+            } 
+
+            // ---
+
+            var f = (extractor && (typeof extractor) == "function")
+                 ? extractor : function(p) { return p };
+
+            var ps = [];
+            for (var i = 0; i < l; i++) {
+                ps.push(f(pairs[i]))
+            }
+
+            return ps
+        };
+
+        var load = function(g, ps) {
+            var p, id = g.attr("id"), b = $(".btn", g), l = ps.length;
+            for (var u = $("ul", g), i = 0; i < l; i++) {
+                p = ps[i];
+                
+                $('<a href="' +p[0]+ '" title="' +p[1]+ '">' +p[1]+ '</a>').
+                    appendTo($('<li></li>').appendTo(u)).click(function() { 
+                        var ret = selectValue(id, g, b, $(this));
+                        b.dropdown('toggle');
+                        return ret
+                    })
+                
+            }
+        };
+
+        if (arg && arg['action'] == "load") {
+            var cb = $(this);
+
+            if (cb.length == 0) {
+                return cb;
+            }
+
+            // ---
+
+            var pairs = options(arg['pairs'], arg['extractor']);
+
+            cb.each(function(i,c) { load($(c), pairs) })
+
+            return cb
+        }
+
+        if (arg && arg['action'] == "reset") {
+            var cb = $(this);
+
+            if (cb.length == 0) {
+                return cb;
+            }
+
+            // ---
+
+            var pairs = options(arg['pairs'], arg['extractor']);
+
+            cb.each(function(i,c) { 
+                var g = $(c), id = g.attr("id"), b = $(".btn", g),
+                v = selectedOption(g)[0];
+
+                removeAll(g);
+                load(g, pairs);
+
+                var o = $("li a[href='"+v+"']", g);
+                selectValue(id, g, b, (o.length == 1) ? o : null)
+            })
+
+            return cb
+        }
+
+        // ---
+
+        // Setup
+        $(this).each(function(i,e) {
+            var s = $(e), id = s.attr("id"),
+            b = $('<button type="button" class="btn dropdown-toggle" data-toggle="dropdown"><span class="label"></span><span class="caret"></span></button>').attr("name", s.attr("name")).attr("id", id+"-tog"), lb= $(".label", b),
+            l = $('<ul class="dropdown-menu" role="menu"></ul>');
+
+            $("option", s).each(function(j,f) {
+                var o = $(f);
+                $('<li><a href="' +o.val()+ '">' +o.text()+ '</a></li>').appendTo(l)
+            });
+
+            $('<input type="hidden" name="' +s.attr("name")+ 
+              '" id="' +id+ '-hid" value="" />').insertAfter(s);
+
+            var g = $('<div class="btn-group ugc-form-control ugc-combobox"></div>').attr("id", id).append(b).append(l), so = selectedOption(g);
+
+            selectValue(id, g, b, so[2]);
+
+            $("li > a", g).click(function() { 
+                var a = $(this), ret = selectValue(id, g, b, a);
+                b.dropdown('toggle');
+                return ret
+            });
+            
+            b.click(function() { $("ul", g).width(g.width()) });
+
+            s.replaceWith(g)
+        })
+    };
+})(jQuery);
